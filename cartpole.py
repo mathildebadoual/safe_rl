42ed877cbe087a5c9ca9df86c155a9482cff6eca
Mathilde Badoual <mathilde.badoual@gmail.com>
2019-05-05 18:21:20 -0700

    add class discrete

--- cartpole.py
+++ cartpole.py
@@ -1,16 +1,10 @@
-"""
-Classic cart-pole system implemented by Rich Sutton et al.
-Copied from http://incompleteideas.net/sutton/book/code/pole.c
-permalink: https://perma.cc/C9ZM-652R
-"""
-
 import math
+import random
+
 import gym
-from gym import spaces, logger
-from gym.utils import seeding
 import numpy as np
-import control
-from simple_pid import PID
+from gym import logger, spaces
+from gym.utils import seeding
 
 
 class CartPoleEnv():
@@ -21,25 +15,22 @@ class CartPoleEnv():
         self.total_mass = (self.masspole + self.masscart)
         self.length = 0.5  # actually half the pole's length
         self.polemass_length = (self.masspole * self.length)
-        self.force_mag = 10.0
         self.tau = 0.02  # seconds between state updates
 
         # Angle at which to fail the episode
         # self.theta_threshold_radians = 12 * 2 * math.pi / 360
         # self.x_threshold = 2.4
-        self.theta_threshold_radians = 12 * 100 * math.pi / 360
+        self.theta_threshold_sradians = 12 * 100 * math.pi / 360
         self.x_threshold = 2.4 * 100
 
         self.optimal_lqr_controller = self.compute_lqr_controller()
 
-        # Angle limit set to 2 * theta_threshold_radians so failing observation is still within bounds
         high = np.array([
             self.x_threshold * 2,
             np.finfo(np.float32).max,
             self.theta_threshold_radians * 2,
             np.finfo(np.float32).max])
 
-        self.action_space = spaces.Box(low=-100, high=100, shape=(1,), dtype=np.float32)
         self.observation_space = spaces.Box(-high, high)
 
         self.seed()
@@ -48,41 +39,38 @@ class CartPoleEnv():
         self.previous_state = None
         self.previous_action = None
 
-        self.pid = PID(1, 0.1, 0.05, setpoint=1)
-
         self.steps_beyond_done = None
 
-    def seed(self, seed=None):
-        self.np_random, seed = seeding.np_random(seed)
-        return [seed]
+    def step(self):
+        raise NotImplementedError
 
-    def step(self, action):
-        state = self.state
-        action = - np.dot(self.optimal_lqr_controller, state) + action
-        if self.previous_action is None:
-            self.previous_action = action
-        # assert self.action_space.contains(action), "%r (%s) invalid" % (action, type(action))
+    def dynamics_step(self, state, force):
         x, x_dot, theta, theta_dot = state
         costheta = math.cos(theta)
         sintheta = math.sin(theta)
-        temp = (action[0] + self.polemass_length * theta_dot * theta_dot * sintheta) / self.total_mass
+        temp = (force + self.polemass_length * theta_dot * theta_dot *
+                sintheta) / self.total_mass
         thetaacc = (self.gravity * sintheta - costheta * temp) / (
-                self.length * (4.0 / 3.0 - self.masspole * costheta * costheta / self.total_mass))
-        xacc = temp - self.polemass_length * thetaacc * costheta / self.total_mass
+            self.length * (4.0 / 3.0 - self.masspole * costheta *
+                           costheta / self.total_mass))
+        xacc = temp - self.polemass_length * thetaacc * costheta / \
+            self.total_mass
         x = x + self.tau * x_dot
         x_dot = x_dot + self.tau * xacc
         theta = theta + self.tau * theta_dot
         theta_dot = theta_dot + self.tau * thetaacc
         self.state = (x, x_dot, theta, theta_dot)
         done = x < -self.x_threshold \
-               or x > self.x_threshold \
-               or theta < -self.theta_threshold_radians \
-               or theta > self.theta_threshold_radians
+            or x > self.x_threshold \
+            or theta < -self.theta_threshold_radians \
+            or theta > self.theta_threshold_radians
         done = bool(done)
 
         if not done:
-            reward = 1.0 * (1 + float(
-                self.compute_cost(np.array(self.state), np.array(self.previous_state), action, self.previous_action)))
+            reward = float(
+                self.compute_cost(
+                    np.array(self.state),
+                    force))
         elif self.steps_beyond_done is None:
             # Pole just fell!
             self.steps_beyond_done = 0
@@ -90,19 +78,23 @@ class CartPoleEnv():
         else:
             if self.steps_beyond_done == 0:
                 logger.warn(
-                    "You are calling 'step()' even though this environment has already returned done = True. You should always call 'reset()' once you receive 'done = True' -- any further steps are undefined behavior.")
+                    "You are calling 'step()' even though this environment "
+                    "has already returned done = True. You should always call "
+                    "'reset()' once you receive 'done = True' -- any further "
+                    "steps are undefined behavior.")
             self.steps_beyond_done += 1
             reward = 0.0
 
-        self.previous_state = self.state
-        self.previous_action = action
+        return done, reward
 
-        return np.array(self.state), reward, done, {'action_lqr': - np.dot(self.optimal_lqr_controller, state)}
+    def seed(self, seed=None):
+        self.np_random, seed = seeding.np_random(seed)
+        return [seed]
 
     def reset(self, init_state=None):
         if init_state is None:
             # self.state = self.observation_space.sample()
-            self.state = self.np_random.uniform(low=-3, high=3, size=(4,))
+            self.state = np.array([0, 0, random.uniform(-0.1, 0.1), 0])
         else:
             self.state = init_state
         self.previous_state = self.state
@@ -125,13 +117,15 @@ class CartPoleEnv():
         if self.viewer is None:
             from gym.envs.classic_control import rendering
             self.viewer = rendering.Viewer(screen_width, screen_height)
-            l, r, t, b = -cartwidth / 2, cartwidth / 2, cartheight / 2, -cartheight / 2
+            l, r, t, b = -cartwidth / 2, cartwidth / 2, cartheight / 2, \
+                         -cartheight / 2
             axleoffset = cartheight / 4.0
             cart = rendering.FilledPolygon([(l, b), (l, t), (r, t), (r, b)])
             self.carttrans = rendering.Transform()
             cart.add_attr(self.carttrans)
             self.viewer.add_geom(cart)
-            l, r, t, b = -polewidth / 2, polewidth / 2, polelen - polewidth / 2, -polewidth / 2
+            l, r, t, b = -polewidth / 2, polewidth / 2, polelen - \
+                polewidth / 2, -polewidth / 2
             pole = rendering.FilledPolygon([(l, b), (l, t), (r, t), (r, b)])
             pole.set_color(.8, .6, .4)
             self.poletrans = rendering.Transform(translation=(0, axleoffset))
@@ -147,7 +141,8 @@ class CartPoleEnv():
             self.track.set_color(0, 0, 0)
             self.viewer.add_geom(self.track)
 
-        if self.state is None: return None
+        if self.state is None:
+            return None
 
         x = self.state
         cartx = x[0] * scale + screen_width / 2.0  # MIDDLE OF CART
@@ -157,19 +152,51 @@ class CartPoleEnv():
         return self.viewer.render(return_rgb_array=mode == 'rgb_array')
 
     def close(self):
-        if self.viewer: self.viewer.close()
+        if self.viewer:
+            self.viewer.close()
 
-    def compute_lqr_controller(self):
-        A = np.array(
-            [[0, 1, 0, 0], [0, 0, -self.masspole * self.gravity / self.masscart, 0], [0, 0, 0, 1],
-             [0, 0, self.total_mass * self.gravity / (self.length * self.masscart), 0]])
-        B = np.array([[0], [1 / self.masscart], [0], [-1 / (self.length * self.masscart)]])
-        Q = np.eye(4)
-        R = 0.1
-        K, _, _ = control.lqr(A, B, Q, R)
-        return K
-
-    def compute_cost(self, state, previous_state, action, previous_action):
-        Q = np.eye(4)
-        R = 0.1
+    def compute_cost(self, state, action):
+        Q = 10 * np.eye(4)
+        R = 0.01
         return - np.dot(state.T, np.dot(Q, state)) - R * action ** 2
+
+
+class CartPoleEnvContinous(CartPoleEnv):
+    def __init__(self):
+        super(CartPoleEnvDiscrete, self).__init__()
+        self.action_space = spaces.Box(low=-1, high=1, shape=(1,),
+                                       dtype=np.float32)
+
+    def step(self, action):
+        state = self.state
+        if self.previous_action is None:
+            self.previous_action = action
+        assert self.action_space.contains(action), "invalid action"
+
+        done, reward = self.dynamics_step(state, action)
+
+        self.previous_state = self.state
+        self.previous_action = action
+
+        return np.array(self.state), reward, done
+
+
+class CartPoleEnvDiscrete(CartPoleEnv):
+    def __init__(self):
+        super(CartPoleEnvDiscrete, self).__init__()
+        self.force_mag = 1
+        self.action_space = spaces.Discrete(2)
+
+    def step(self, action):
+        state = self.state
+        if self.previous_action is None:
+            self.previous_action = action
+        assert self.action_space.contains(action), "invalid action"
+        force = self.force_mag if action == 1 else -self.force_mag
+
+        done, reward = self.dynamics_step(state, force)
+
+        self.previous_state = self.state
+        self.previous_action = action
+
+        return np.array(self.state), reward, done
